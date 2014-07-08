f50ce457d521c5fc71c46881b4dc295925d908d7
Pothi Kalimuthu <pothi@tinywp.com>
2014-07-08 23:26:53 +0000

    Exclude all plugins

--- wp-content/mu-plugins/login-with-ajax.php
+++
@@ -1,2 +0,0 @@
-<?php // mu-plugins/load.php
-require WPMU_PLUGIN_DIR.'/login-with-ajax/login-with-ajax.php';
--- wp-content/plugins/social-connect/constants.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if( !defined( 'SOCIAL_CONNECT_PLUGIN_URL' )) {
-  define( 'SOCIAL_CONNECT_PLUGIN_URL', plugins_url() . '/' . basename( dirname( __FILE__ )));
-}
-
